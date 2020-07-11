5573a01d5a539b2b9f972647fa6d6895b5ce388e
tabtoyou <gxq9106@gmail.com>
2020-07-12 00:30:12 +0900

    test

---
+++ assignments/assignment1/knn_test.py
@@ -0,0 +1,246 @@
+# Run some setup code for this notebook.
+
+import random
+import numpy as np
+from cs231n.data_utils import load_CIFAR10
+#from keras.datasets import cifar10 
+import matplotlib.pyplot as plt
+
+# This is a bit of magic to make matplotlib figures appear inline in the notebook
+# rather than in a new window.
+plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+# Some more magic so that the notebook will reload external python modules;
+# see http://stackoverflow.com/questions/1907993/autoreload-of-modules-in-ipython
+#%load_ext autoreload
+#%autoreload 2
+
+# Load the raw CIFAR-10 data.
+cifar10_dir = 'cs231n/datasets/cifar-10-batches-py'
+
+# Cleaning up variables to prevent loading data multiple times (which may cause memory issue)
+
+try:
+   del X_train, y_train
+   del X_test, y_test
+   print('Clear previously loaded data.')
+except:
+   pass
+
+X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
+'''
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+'''
+# As a sanity check, we print out the size of the training and test data.
+print('Training data shape: ', X_train.shape)
+print('Training labels shape: ', y_train.shape)
+print('Test data shape: ', X_test.shape)
+print('Test labels shape: ', y_test.shape)
+
+
+
+
+# Subsample the data for more efficient code execution in this exercise
+num_training = 1000
+mask = list(range(num_training))
+X_train = X_train[mask]
+y_train = y_train[mask]
+
+num_test = 200
+mask = list(range(num_test))
+X_test = X_test[mask]
+y_test = y_test[mask]
+
+# Reshape the image data into rows
+X_train = np.reshape(X_train, (X_train.shape[0], -1))
+X_test = np.reshape(X_test, (X_test.shape[0], -1))
+print(X_train.shape, X_test.shape)
+
+
+
+from cs231n.classifiers import KNearestNeighbor
+
+# Create a kNN classifier instance. 
+# Remember that training a kNN classifier is a noop: 
+# the Classifier simply remembers the data and does no further processing 
+classifier = KNearestNeighbor()
+classifier.train(X_train, y_train)
+
+'''
+
+# Open cs231n/classifiers/k_nearest_neighbor.py and implement
+# compute_distances_two_loops.
+
+# Test your implementation:
+dists = classifier.compute_distances_two_loops(X_test)
+#print(dists.shape)
+#print(dists)
+
+
+
+# Now implement the function predict_labels and run the code below:
+# We use k = 1 (which is Nearest Neighbor).
+Yte_predict = classifier.predict_labels(dists, k=5)
+
+count = 0
+for i in range(len(Yte_predict)):
+  if Yte_predict[i] == y_test[i]:
+    count +=1
+# and now print the classification accuracy, which is the average number
+# of examples that are correctly predicted (i.e. label matches)
+print('accuracy: %f' %(count/len(Yte_predict)))
+'''
+
+
+'''
+# Compute and print the fraction of correctly predicted examples
+num_correct = np.sum(y_test_pred == y_test)
+accuracy = float(num_correct) / num_test
+print('Got %d / %d correct => accuracy: %f' % (num_correct, num_test, accuracy))
+'''
+
+'''
+# Now lets speed up distance matrix computation by using partial vectorization
+# with one loop. Implement the function compute_distances_one_loop and run the
+# code below:
+dists_one = classifier.compute_distances_one_loop(X_test)
+
+# To ensure that our vectorized implementation is correct, we make sure that it
+# agrees with the naive implementation. There are many ways to decide whether
+# two matrices are similar; one of the simplest is the Frobenius norm. In case
+# you haven't seen it before, the Frobenius norm of two matrices is the square
+# root of the squared sum of differences of all elements; in other words, reshape
+# the matrices into vectors and compute the Euclidean distance between them.
+difference = np.linalg.norm(dists - dists_one, ord='fro')
+print('One loop difference was: %f' % (difference, ))
+if difference < 0.001:
+    print('Good! The distance matrices are the same')
+else:
+    print('Uh-oh! The distance matrices are different')
+'''
+'''
+# Now implement the fully vectorized version inside compute_distances_no_loops
+# and run the code
+dists_two = classifier.compute_distances_no_loops(X_test)
+
+# check that the distance matrix agrees with the one we computed before:
+difference = np.linalg.norm(dists - dists_two, ord='fro')
+print('No loop difference was: %f' % (difference, ))
+if difference < 0.001:
+    print('Good! The distance matrices are the same')
+else:
+    print('Uh-oh! The distance matrices are different')
+
+'''
+'''
+
+# Let's compare how fast the implementations are
+def time_function(f, *args):
+    """
+    Call a function f with args and return the time (in seconds) that it took to execute.
+    """
+    import time
+    tic = time.time()
+    f(*args)
+    toc = time.time()
+    return toc - tic
+
+two_loop_time = time_function(classifier.compute_distances_two_loops, X_test)
+print('Two loop version took %f seconds' % two_loop_time)
+
+one_loop_time = time_function(classifier.compute_distances_one_loop, X_test)
+print('One loop version took %f seconds' % one_loop_time)
+
+no_loop_time = time_function(classifier.compute_distances_no_loops, X_test)
+print('No loop version took %f seconds' % no_loop_time)
+
+# You should see significantly faster performance with the fully vectorized implementation!
+
+# NOTE: depending on what machine you're using, 
+# you might not see a speedup when you go from two loops to one loop, 
+# and might even see a slow-down.
+
+'''
+
+
+num_folds = 5
+k_choices = [1, 3, 5, 8, 10, 12, 15, 20, 50, 100]
+
+X_train_folds = []
+y_train_folds = []
+################################################################################
+# TODO:                                                                        #
+# Split up the training data into folds. After splitting, X_train_folds and    #
+# y_train_folds should each be lists of length num_folds, where                #
+# y_train_folds[i] is the label vector for the points in X_train_folds[i].     #
+# Hint: Look up the numpy array_split function.                                #
+################################################################################
+# *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+X_train_folds = np.array_split(X_train,num_folds)
+y_train_folds = np.array_split(y_train,num_folds)
+
+# *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+# A dictionary holding the accuracies for different values of k that we find
+# when running cross-validation. After running cross-validation,
+# k_to_accuracies[k] should be a list of length num_folds giving the different
+# accuracy values that we found when using that value of k.
+k_to_accuracies = {}
+
+
+################################################################################
+# TODO:                                                                        #
+# Perform k-fold cross validation to find the best value of k. For each        #
+# possible value of k, run the k-nearest-neighbor algorithm num_folds times,   #
+# where in each case you use all but one of the folds as training data and the #
+# last fold as a validation set. Store the accuracies for all fold and all     #
+# values of k in the k_to_accuracies dictionary.                               #
+################################################################################
+# *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+for j in k_choices:
+   k_to_accuracies[j] = []
+   for i in range(num_folds):
+
+      X_Train = np.append(X_train_folds[0:i], X_train_folds[i+1:]).reshape((X_train.shape[0] - int(X_train.shape[0]/num_folds), -1))
+      y_Train = np.append(y_train_folds[0:i], y_train_folds[i+1:]).reshape((y_train.shape[0] - int(y_train.shape[0]/num_folds), -1))
+      classifier.train(X_Train, y_Train)
+      
+      dists = classifier.compute_distances_two_loops(X_train_folds[i])
+      y_pred = classifier.predict_labels(dists,k=j)
+
+      count = 0
+      for i in range(len(y_pred)):
+         if y_pred[i] == y_test[i]:
+            count +=1
+      
+      acc = count/len(y_pred)
+      k_to_accuracies[j].append(acc)
+      # and now print the classification accuracy, which is the average number
+      # of examples that are correctly predicted (i.e. label matches)
+      
+
+# *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+# Print out the computed accuracies
+for k in sorted(k_to_accuracies):
+    for accuracy in k_to_accuracies[k]:
+        print('k = %d, accuracy = %f' % (k, accuracy))
+
+
+# plot the raw observations
+for k in k_choices:
+    accuracies = k_to_accuracies[k]
+    plt.scatter([k] * len(accuracies), accuracies)
+
+# plot the trend line with error bars that correspond to standard deviation
+accuracies_mean = np.array([np.mean(v) for k,v in sorted(k_to_accuracies.items())])
+accuracies_std = np.array([np.std(v) for k,v in sorted(k_to_accuracies.items())])
+plt.errorbar(k_choices, accuracies_mean, yerr=accuracies_std)
+plt.title('Cross-validation on k')
+plt.xlabel('k')
+plt.ylabel('Cross-validation accuracy')
+plt.show()
---
+++ assignments/assignment2/pytorch_test.py
@@ -0,0 +1,41 @@
+def three_layer_convnet(x, params):
+    """
+    Performs the forward pass of a three-layer convolutional network with the
+    architecture defined above.
+
+    Inputs:
+    - x: A PyTorch Tensor of shape (N, 3, H, W) giving a minibatch of images
+    - params: A list of PyTorch Tensors giving the weights and biases for the
+      network; should contain the following:
+      - conv_w1: PyTorch Tensor of shape (channel_1, 3, KH1, KW1) giving weights
+        for the first convolutional layer
+      - conv_b1: PyTorch Tensor of shape (channel_1,) giving biases for the first
+        convolutional layer
+      - conv_w2: PyTorch Tensor of shape (channel_2, channel_1, KH2, KW2) giving
+        weights for the second convolutional layer
+      - conv_b2: PyTorch Tensor of shape (channel_2,) giving biases for the second
+        convolutional layer
+      - fc_w: PyTorch Tensor giving weights for the fully-connected layer. Can you
+        figure out what the shape should be?
+      - fc_b: PyTorch Tensor giving biases for the fully-connected layer. Can you
+        figure out what the shape should be?
+    
+    Returns:
+    - scores: PyTorch Tensor of shape (N, C) giving classification scores for x
+    """
+    conv_w1, conv_b1, conv_w2, conv_b2, fc_w, fc_b = params
+    scores = None
+    ################################################################################
+    # TODO: Implement the forward pass for the three-layer ConvNet.                #
+    ################################################################################
+    # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+    N,_,H,W = x.shape
+    socres = F.conv2d
+
+
+    # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    ################################################################################
+    #                                 END OF YOUR CODE                             #
+    ################################################################################
+    return scores
